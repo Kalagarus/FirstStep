190079d355e4ca9891fcdc3d7c9473faa6cca2b1
Kalagarus <kalagarus@gmail.com>
2019-01-27 20:49:43 +0700

    Fuck you spielberg

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(4 / 0);
+console.log("string" * 9);
+
+let person = {
+  name: "john",
+  age: 25,
+  isMarried: true
+};
+
+console.log(person.name);
+
+let arr = ["greed.jpg", "blue.bmp", "yellow.png"];
+
+console.log(arr[2]);
+
+// alert("Hellow World");
+
+// let assQuestion = confirm("Are you YOBA?");
+//
+// console.log(assQuestion);
+//
+// let assQuestion = prompt("Are you YOBA?", "Yeah");
+//
+// console.log(assQuestion);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
